777b8eb5c294e9117a1c0151c4bdfa992693f255
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-12 09:58:00 +0800

    nodebasic
    export server function

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http')
 
-http.createServer(function(req, res){
-	response.writhHead(200, {'content-Type': 'text/plain'})
-	response.write('Hello World')
-	response.end()
-}).listen(8888)
+const start = function(){
+	http.createServer(function(req, res){
+		console.log('Request received')
+		res.writeHead(200, {'content-Type': 'text/plain'})
+		res.write('Hello World')
+		res.end()
+	}).listen(8888)
+
+	console.log('server start!')
+}
+
+exports.start = start
