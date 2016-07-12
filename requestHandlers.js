e0da621d8fdac103d181897b3e2e83a40c15dfc5
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-12 10:33:11 +0800

    nodebasic
    response simple html

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,22 @@ var exec = require("child_process").exec;
 function start(res){
 	console.log('Request handler "start" was called')	
 
-	exec("ls -lah", function (error, stdout, stderr) {
-	  	res.writeHead(200, {'content-Type': 'text/plain'})
-		res.write(stdout)
-		res.end()
-	});
+	var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {'content-Type': 'text/html'})
+	res.write(body)
+	res.end()
 }
 
 function upload(res){
