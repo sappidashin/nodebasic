f9d9e3c060644443e9ad43d0ea8ed482bce15c26
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2016-07-12 10:05:33 +0800

    nodebasic
    add router

---
+++ router.js
@@ -0,0 +1,5 @@
+const route = function(pathname){
+	console.log('About to route a request for ' + pathname)
+}
+
+exports.route = route
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http')
+var url = require('url')
 
-const start = function(){
+const start = function(route){
 	http.createServer(function(req, res){
-		console.log('Request received')
+		var pathname = url.parse(req.url).pathname;
+		console.log('Request for ' + pathname + ' received!')
+
+		route(pathname)
+
 		res.writeHead(200, {'content-Type': 'text/plain'})
 		res.write('Hello World')
 		res.end()
